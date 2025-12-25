4510523711dca56f72025e37c740492c0660578f
Colin Gilbert <colingilbert86@gmail.com>
2025-12-24 20:38:46 -0600

    tentative fix

--- src/animgraph.rs
+++ src/animgraph.rs
@@ -881,7 +881,6 @@ impl AnimGraph {
         }
 
         let mut finished = false;
-
         let mut nodes_to_evaluate = Vec::<NodeIndex>::new();
         let mut next_nodes = HashSet::<NodeIndex>::new();
         while !finished {
@@ -921,12 +920,12 @@ impl AnimGraph {
 
             // next_nodes.clear();
 
-            if self.state_machine_nodes[state_machine_pool_idx]
-                .trackers
-                .len()
-                == 0
-            {
-           //if next_nodes.len() == 0 {
+            // if self.state_machine_nodes[state_machine_pool_idx]
+            //     .trackers
+            //     .len()
+            //     == 0
+            // {
+           if next_nodes.len() == 0 {
                 println!("FINISHED EVALUATING STATE MACHINE");
                 finished = true;
             }
