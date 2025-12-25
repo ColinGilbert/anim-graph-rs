b64ecef0d847531e31d1354be94f77d3d730c431
Colin Gilbert <colingilbert86@gmail.com>
2025-12-24 20:26:10 -0600

    debug

--- src/animgraph.rs
+++ src/animgraph.rs
@@ -865,6 +865,7 @@ impl AnimGraph {
 
         match start {
             AnimNode::Start => {
+                println!("START NODE FOUND")
                 for out in self
                     .graph
                     .outputs(self.state_machine_nodes[state_machine_pool_idx].start)
